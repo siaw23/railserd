a622869f2a0501c9f57e505baa4ee9aa65929ce5
Emmanuel Hayford <hi@hayford.dev>
2025-09-24 15:47:03 +0200

    Add lucide icons

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import { createIcons, FileText } from "lucide"
+
+document.addEventListener("turbo:load", () => {
+  try {
+    createIcons({ icons: { FileText } })
+  } catch (e) { /* ignore if not on page */ }
+})
