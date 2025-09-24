72bc32deb3dbe745a15275069287fd785b9c6e71
Emmanuel Hayford <hi@hayford.dev>
2025-09-24 18:43:22 +0200

    Fix prod CSS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,9 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
+  # Serve precompiled static assets (public/assets)
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,4 +4,7 @@
 Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join("app/assets/builds")
+
+# Precompile Tailwind output for production
+Rails.application.config.assets.precompile += %w[ tailwind.css ]
